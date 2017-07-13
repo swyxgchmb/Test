e6a839431b6afce51c5f5dbbc8424d4922f12ac8
huangmb <huangmb@HUANGMB226>
2017-07-13 23:05:03 +0800

    添加项目文件。

---
+++ CancellationTokenSorceTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellationTokenSorceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notifier = new CancellationTokenSource();
+            int a = 10;
+            Task.Factory.StartNew(() =>
+            {
+                while (!notifier.IsCancellationRequested)
+                {
+                    Thread.Sleep(30);
+                }
+                Console.WriteLine("Enter Here");
+            }, notifier.Token);
+            notifier.Token.Register(() =>
+            {
+                Thread.Sleep(1100);
+                Console.WriteLine(a);
+            });
+
+            Thread.Sleep(1000);
+            notifier.Cancel();
+            Console.ReadLine();
+        }
+    }
+}
